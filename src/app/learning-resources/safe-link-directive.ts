0edf28497f6ca0eb48a0f101cb0b14345ccf02a9
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-14 02:30:46 +0300

    feat(safe-link): Use Attribute Directives to change element behavior

--- src/app/learning-resources/safe-link-directive.ts
+++ src/app/learning-resources/safe-link-directive.ts
@@ -3,9 +3,24 @@ import { Directive } from '@angular/core';
 @Directive({
   selector: 'a[appSafeLink]',
   standalone: true,
+  host: {
+    '(click)': 'onConfirmLeavePage($event)',
+  },
 })
 export class SafeLinkDirective {
   constructor() {
     console.log('SaveLinkDirective is active!');
+  }
+
+  onConfirmLeavePage(event: MouseEvent) {
+    const wantsToLeave = window.confirm(
+      'Are you sure you want to leave this page?'
+    );
+
+    if (wantsToLeave) {
+      return;
+    }
+
+    event.preventDefault();
   }
 }
