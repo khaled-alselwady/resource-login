fc86c45f7df6b0d7f35f80869553c8b4c954af98
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-16 11:34:06 +0300

    feat(safe-link): use input to change the query param dynamically

--- src/app/learning-resources/safe-link-directive.ts
+++ src/app/learning-resources/safe-link-directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, input } from '@angular/core';
 
 @Directive({
   selector: 'a[appSafeLink]',
@@ -8,6 +8,8 @@ import { Directive } from '@angular/core';
   },
 })
 export class SafeLinkDirective {
+  queryParam = input('myapp');
+
   constructor() {
     console.log('SaveLinkDirective is active!');
   }
@@ -18,6 +20,9 @@ export class SafeLinkDirective {
     );
 
     if (wantsToLeave) {
+      const address = (event.target as HTMLAnchorElement).href;
+      (event.target as HTMLAnchorElement).href =
+        address + '?from=' + this.queryParam();
       return;
     }
 
