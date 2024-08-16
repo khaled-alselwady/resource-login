24d46c11a87708efb7d2deb07731ac0f1007c979
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-17 00:05:00 +0300

    feat(safe-link): use Structural Directive by ng-template element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { AuthComponent } from './auth/auth.component';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
 import { AuthService } from './auth/auth.service';
 import { NgIf } from '@angular/common';
+import { AuthDirective } from './auth/auth.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AuthComponent, LearningResourcesComponent, NgIf],
+  imports: [AuthComponent, LearningResourcesComponent, NgIf, AuthDirective],
 })
 export class AppComponent {
   private authService = inject(AuthService);
--- src/app/learning-resources/learning-resources.component.ts
+++ src/app/learning-resources/learning-resources.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { SafeLinkDirective } from './safe-link-directive';
+import { SafeLinkDirective } from './safe-link.directive';
 
 @Component({
   selector: 'app-learning-resources',
